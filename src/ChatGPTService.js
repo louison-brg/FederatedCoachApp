8744d4aa356125e6d8aebf1cc7665f4efb5b1190
louison-brg <beranger.louison@yahoo.fr>
2024-05-24 18:50:49 +0200

    fix directory

--- src/ChatGPTService.js
+++ src/ChatGPTService.js
@@ -1,7 +1,7 @@
 // src/ChatGPTService.js
 import axios from 'axios';
 
-//const API_KEY = ''; // Replace with your actual API key
+// api key
 
 const instance = axios.create({
     baseURL: 'https://api.openai.com/v1/', // Ensure this is correct
